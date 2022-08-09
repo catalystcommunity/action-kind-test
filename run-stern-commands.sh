123c7fc547a6f9007e1a6595681026c7e49d1b96
zack stevens <zack.st7@gmail.com>
2022-08-09 11:04:42 -0600

    fix: Add stern commands to run on failure

--- run-stern-commands.sh
+++ run-stern-commands.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
-# split arg into array of strings split on a comma
-echo "received commands"
-echo "$1"
-IFS='\n' read -ra COMMANDS <<< "$1"
-echo "COMMANDS is $COMMANDS"
+# split arg into array of strings separated by newlines
+commands=()
+readarray -t commands <<<$1
 # run commands, checking them against a regex to be sure they're actually stern commands
-for i in "${COMMANDS[@]}"; do
+for i in "${commands[@]}"; do
   if [[ "$i" =~ ^stern.* ]]; then
-    echo "Running stern command $i"
+    echo "Running stern command: $i"
     # run with a timeout because stern will tail forever if you don't, so this will print logs for 10 seconds then exit
 #    timeout 10 $i
   else
     # don't run arbitrary commands
-    echo "Invalid stern command $i , not running command"
+    echo "Invalid stern command: $i , not running command"
   fi
 done
