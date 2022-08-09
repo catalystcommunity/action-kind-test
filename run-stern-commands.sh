b8baafa7e73dc28fc0c49e04d6597ef4ebb1a819
Zack Stevens <zack.st7@gmail.com>
2022-08-09 12:06:19 -0600

    fix: Support running stern commands on failure to enable easier test troubleshooting (#7)

---
+++ run-stern-commands.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# split arg into array of strings separated by newlines
+commands=()
+readarray -t commands <<<"$1"
+# run commands, checking them against a regex to be sure they're actually stern commands
+for i in "${commands[@]}"; do
+  if [[ "$i" =~ ^stern.* ]]; then
+    echo "Running stern command: $i"
+    # run with a timeout because stern will tail forever if you don't, so this will print logs for 5 seconds then exit
+    timeout 5 $i
+  else
+    # don't run arbitrary commands
+    echo "Invalid stern command: $i , not running command"
+  fi
+done
